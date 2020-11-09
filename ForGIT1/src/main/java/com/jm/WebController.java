8b29586a750875397a850bebe4f2cac28118b466
jmaji <dxc51jab4021@datascience.manipal.edu>
2020-11-09 12:07:46 +0530

    first commit

---
+++ ForGIT1/src/main/java/com/jm/WebController.java
@@ -0,0 +1,13 @@
+package com.jm;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	@GetMapping("/hi")
+	public String sayHello() {
+		return "Hello World ..... From JENKINS";
+	}
+}
